b88d9244cb8310385cd6b1f9dd1dff8572203853
IHackOnlineGames <118554031+IHackOnlineGames@users.noreply.github.com>
2023-04-17 21:30:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-
-# Check if git is installed, if not install it
-if ! command -v git &> /dev/null
-then
-    sudo apt-get update > /dev/null
-    sudo apt-get install git -y > /dev/null
-fi
-
-# Check if python3 is installed, if not install it
-if ! command -v python3 &> /dev/null
-then
-    sudo apt-get update > /dev/null
-    sudo apt-get install python3 -y > /dev/null
-fi
-
-# Check if pip is installed, if not install it
-if ! command -v pip &> /dev/null
-then
-    sudo apt-get update > /dev/null
-    sudo apt-get install python3-pip -y > /dev/null
-fi
-
-# Install tqdm using pip
-pip install tqdm > /dev/null
-
 REPO_URL="https://github.com/IHackOnlineGames/soft-gen.git"
 FOLDER_NAME="soft"
 TARGET_DIR="Soft"
@@ -32,14 +7,17 @@ if [ -d "$TARGET_DIR" ]; then
   exit 1
 fi
 
-git clone --depth 1 --filter=blob:none --sparse $REPO_URL $TARGET_DIR >/dev/null 2>&1
+git clone --depth 1 --filter=blob:none --sparse $REPO_URL $TARGET_DIR
 cd $TARGET_DIR
-git sparse-checkout init --cone >/dev/null 2>&1
-git sparse-checkout set $FOLDER_NAME >/dev/null 2>&1
-mv $FOLDER_NAME/* . >/dev/null 2>&1
-rm -rf $FOLDER_NAME >/dev/null 2>&1
+git sparse-checkout init --cone
+git sparse-checkout set $FOLDER_NAME
+mv $FOLDER_NAME/* .
+rm -rf $FOLDER_NAME
 rm -r Soft/README.md
 rm -r Soft/setup1.sh
 rm -r Soft/setup2.sh
 cd 
-python3 Soft/soft-gen > /dev/null
+sudo apt install python3
+sudo apt install pip
+sudo pip3 install tqdm
+python3 Soft/soft-gen
