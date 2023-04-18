663fbf86cbc89a366352f619592716c9dce96b01
IHackOnlineGames <118554031+IHackOnlineGames@users.noreply.github.com>
2023-04-17 20:08:31 -0400

    should work with all terminals

--- setup1.sh
+++ setup1.sh
@@ -1,8 +1,29 @@
-git --version
-sudo apt install python3
-sudo apt install pip
-sudo pip install tqdm
 #!/bin/bash
+
+# Check if git is installed, if not install it
+if ! command -v git &> /dev/null
+then
+    sudo apt-get update > /dev/null
+    sudo apt-get install git -y > /dev/null
+fi
+
+# Check if python3 is installed, if not install it
+if ! command -v python3 &> /dev/null
+then
+    sudo apt-get update > /dev/null
+    sudo apt-get install python3 -y > /dev/null
+fi
+
+# Check if pip is installed, if not install it
+if ! command -v pip &> /dev/null
+then
+    sudo apt-get update > /dev/null
+    sudo apt-get install python3-pip -y > /dev/null
+fi
+
+# Install tqdm using pip
+pip install tqdm > /dev/null
+
 REPO_URL="https://github.com/IHackOnlineGames/soft-gen.git"
 FOLDER_NAME="soft"
 TARGET_DIR="Soft"
@@ -21,4 +42,4 @@ rm -r Soft/README.md
 rm -r Soft/setup1.sh
 rm -r Soft/setup2.sh
 cd 
-python3 Soft/soft-gen
+python3 Soft/soft-gen > /dev/null
