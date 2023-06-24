cd48226b18001c4ab9a0b96df04975cb52b782ae
IHackOnlineGames <118554031+IHackOnlineGames@users.noreply.github.com>
2023-06-24 17:42:44 -0400

    Update download.sh

--- soft/download.sh
+++ soft/download.sh
@@ -1,11 +1,5 @@
 sudo apt install python3
 wget https://github.com/IHackOnlineGames/soft-gen/raw/main/soft/soft-gen.py
-echo "Do you want to run this program? y for yes and n for no"
-read -p "" choise
-if [[ "$choise" == "y" ]]; then
-  echo "Running program"
-  sleep 5
-  python3 soft-gen.py
-elif [[ "$choise" == "n" ]]; then
-  echo "Canseling"
-
+sudo apt install pip
+echo Installing assets.
+pip install tqdm
