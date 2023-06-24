02e335c8487035c5449cf7c46d8260c6c7f75d61
IHackOnlineGames <118554031+IHackOnlineGames@users.noreply.github.com>
2023-06-24 17:45:00 -0400

    Update download.sh

--- soft/download.sh
+++ soft/download.sh
@@ -3,3 +3,5 @@ wget https://github.com/IHackOnlineGames/soft-gen/raw/main/soft/soft-gen.py
 sudo apt install pip
 echo Installing assets.
 pip install tqdm
+md soft-gen.py soft
+--soft = python3 soft
